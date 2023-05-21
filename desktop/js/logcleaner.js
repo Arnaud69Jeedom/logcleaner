28624cc0da179ebe4fe64c9db2db8e4d15ebb37e
Arnaud69Jeedom <40762752+Arnaud69Jeedom@users.noreply.github.com>
2023-05-21 21:44:45 +0200

    Maj avec Template

--- desktop/js/logcleaner.js
+++ desktop/js/logcleaner.js
@@ -1,21 +1,19 @@
-
 /* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
 
- 
 /* Permet la réorganisation des commandes dans l'équipement */
 $("#table_cmd").sortable({
   axis: "y",
@@ -24,169 +22,66 @@ $("#table_cmd").sortable({
   placeholder: "ui-state-highlight",
   tolerance: "intersect",
   forcePlaceholderSize: true
-});
+})
 
 /* Fonction permettant l'affichage des commandes dans l'équipement */
 function addCmdToTable(_cmd) {
-  //console.log("_cmd:", _cmd);
   if (!isset(_cmd)) {
-    var _cmd = {configuration: {}};
+    var _cmd = {configuration: {}}
   }
   if (!isset(_cmd.configuration)) {
-    _cmd.configuration = {};
+    _cmd.configuration = {}
   }
-  if(!_cmd.logicalId){
-    _cmd.logicalId = 'mode';
-    _cmd.type = 'action';
-    _cmd.subType = 'message';
-  }
-  var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
+  var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">'
+  tr += '<td class="hidden-xs">'
+  tr += '<span class="cmdAttr" data-l1key="id"></span>'
+  tr += '</td>'
+  tr += '<td>'
+  tr += '<div class="input-group">'
+  tr += '<input class="cmdAttr form-control input-sm roundedLeft" data-l1key="name" placeholder="{{Nom de la commande}}">'
+  tr += '<span class="input-group-btn"><a class="cmdAction btn btn-sm btn-default" data-l1key="chooseIcon" title="{{Choisir une icône}}"><i class="fas fa-icons"></i></a></span>'
+  tr += '<span class="cmdAttr input-group-addon roundedRight" data-l1key="display" data-l2key="icon" style="font-size:19px;padding:0 5px 0 0!important;"></span>'
+  tr += '</div>'
+  tr += '<select class="cmdAttr form-control input-sm" data-l1key="value" style="display:none;margin-top:5px;" title="{{Commande info liée}}">'
+  tr += '<option value="">{{Aucune}}</option>'
+  tr += '</select>'
+  tr += '</td>'
+  tr += '<td>'
+  tr += '<span class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType() + '</span>'
+  tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>'
+  tr += '</td>'
+  tr += '<td>'
+  tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="isVisible" checked/>{{Afficher}}</label> '
+  tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="isHistorized" checked/>{{Historiser}}</label> '
+  tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="display" data-l2key="invertBinary"/>{{Inverser}}</label> '
+  tr += '<div style="margin-top:7px;">'
+  tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="minValue" placeholder="{{Min}}" title="{{Min}}" style="width:30%;max-width:80px;display:inline-block;margin-right:2px;">'
+  tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="maxValue" placeholder="{{Max}}" title="{{Max}}" style="width:30%;max-width:80px;display:inline-block;margin-right:2px;">'
+  tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="unite" placeholder="Unité" title="{{Unité}}" style="width:30%;max-width:80px;display:inline-block;margin-right:2px;">'
+  tr += '</div>'
+  tr += '</td>'
   tr += '<td>';
-  tr += '<div class="input-group">';
-  tr += '<span class="cmdAttr" data-l1key="id" style="display : none;"></span>';
-  tr += '<span class="cmdAttr" data-l1key="logicalId" style="display:none;"></span>';
-  tr += '<span class="type" type="' + init(_cmd.type) + '" style="display:none;">' + jeedom.cmd.availableType() + '</span>';
-  tr += '<span class="subType" subType="' + init(_cmd.subType) + '" style="display:none;"></span>';
-
-  tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" placeholder="{{Nom de la commande}}">';
-  tr += '</div>';
+  tr += '<span class="cmdAttr" data-l1key="htmlstate"></span>'; 
   tr += '</td>';
-
-  tr += '<td>';
-  tr += '<div class="input-group">';
-  tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="duration">';
-  tr += '</div>';
-  tr += '</td>';
-
-  tr += '<td>';
-  tr += '<div class="input-group">';
-  tr += '<input class="cmdAttr form-control input-sm" placeholder="#000000" data-l1key="configuration" data-l2key="titleColor">';
-  tr += '</div>';
-  tr += '</td>';
-
-  tr += '<td>';
-  tr += '<div class="input-group">';
-  tr += '<input class="cmdAttr form-control input-sm" placeholder="#000000" data-l1key="configuration" data-l2key="messageColor">';
-  tr += '</div>';
-  tr += '</td>';
-
-  tr += '<td>';
-  tr += '<div class="input-group">';
-  tr += '<input class="cmdAttr form-control input-sm" placeholder="#ffffff" data-l1key="configuration" data-l2key="backgroundColor">';
-  tr += '</div>';
-  tr += '</td>';
-
-  tr += '<td>';
-  tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="url">';
-  tr += '</td>';
-
-  tr += '<td>';
+  tr += '<td>'
   if (is_numeric(_cmd.id)) {
-     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> ';
-     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> Tester</a>';
+    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> '
+    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> Tester</a>'
   }
-  tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i></td>'
-  tr += '</tr>';
-  $('#table_cmd tbody').append(tr);
-  var tr = $('#table_cmd tbody tr').last();
+  tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove" title="{{Supprimer la commande}}"></i></td>'
+  tr += '</tr>'
+  $('#table_cmd tbody').append(tr)
+  var tr = $('#table_cmd tbody tr').last()
   jeedom.eqLogic.buildSelectCmd({
     id:  $('.eqLogicAttr[data-l1key=id]').value(),
     filter: {type: 'info'},
     error: function (error) {
-      $('#div_alert').showAlert({message: error.message, level: 'danger'});
+      $('#div_alert').showAlert({message: error.message, level: 'danger'})
     },
     success: function (result) {
-      tr.find('.cmdAttr[data-l1key=value]').append(result);
-      tr.setValues(_cmd, '.cmdAttr');
-      jeedom.cmd.changeType(tr, init(_cmd.subType));
+      tr.find('.cmdAttr[data-l1key=value]').append(result)
+      tr.setValues(_cmd, '.cmdAttr')
+      jeedom.cmd.changeType(tr, init(_cmd.subType))
     }
-  });
+  })
 }
-  
-
-// $(".eqLogic").delegate(".listCmdInfo", 'click', function () {
-//   var el = $(this).closest('.form-group').find('.eqLogicAttr');
-//   jeedom.cmd.getSelectModal({
-//     cmd: {
-//       type: 'info'
-//     }
-//   }, function (result) {
-//     el.value(result.human);
-//   });
-// });
-
-// $(".eqLogic").delegate(".listCmdAction", 'click', function () {
-//     //console.log("--------- listCmdAction");
-//     var type = $(this).attr('data-type');
-//     var el = $(this).closest('.' + type).find('.expressionAttr[data-l1key=cmd]');
-
-//     jeedom.cmd.getSelectModal({cmd: {type: 'action'}}, function (result) {
-//         el.value(result.human);
-//         jeedom.cmd.displayActionOption(el.value(), '', function (html) {
-//           el.closest('.' + type).find('.actionOptions').html(html);
-//           taAutosize();
-//         });
-//     });
-//   });
-
-// $(".eqLogic").delegate(".listAction", 'click', function () {
-// 	//console.log("--------- listAction");
-//   var type = $(this).attr('data-type');
-//   var el = $(this).closest('.' + type).find('.expressionAttr[data-l1key=cmd]');
-
-//   jeedom.getSelectActionModal({}, function (result) {
-//     el.value(result.human);
-//     jeedom.cmd.displayActionOption(el.value(), '', function (html) {
-//       el.closest('.' + type).find('.actionOptions').html(html);
-//       taAutosize();
-//     });
-//   });
-// });
-
-
-/*** Save ***/
-function saveEqLogic(_eqLogic) {
-  if (!isset(_eqLogic.configuration)) {
-    _eqLogic.configuration = {};
-  }
-  _eqLogic.configuration.Pipup = $('#div_confPipups .confPipup').getValues('.expressionAttr');
-  _eqLogic.configuration.action = $('#div_confActions .confAction').getValues('.expressionAttr');
-
-  // console.log('saveEqLogic:', _eqLogic);
-  return _eqLogic;
-}
-
-function printEqLogic(_eqLogic) {
-  // console.log('printEqLogic:', _eqLogic);
-
-  $('#div_confPipups').empty();
-  if (isset(_eqLogic.configuration)) {
-    if (isset(_eqLogic.configuration.Pipup)) {
-      for (var i in _eqLogic.configuration.Pipup) {
-        //console.log("printEqLogic.addConfPipups", _eqLogic.configuration.Pipup[i]);
-        addConfPipups(_eqLogic.configuration.Pipup[i]);
-      }
-    }
-  }
-
-  $('#div_confActions').empty();
-  if (isset(_eqLogic.configuration)) {
-    if (isset(_eqLogic.configuration.action)) {
-      for (var i in _eqLogic.configuration.action) {
-        //console.log("printEqLogic.addConfActions", _eqLogic.configuration.action[i]);
-        addConfActions(_eqLogic.configuration.action[i]);
-      }
-    }
-  }
-}
-
-
-// Sondes triables
-$("#div_confPipups").sortable({
-  axis: "y",
-  cursor: "move",
-  items: ".confPipup",
-  placeholder: "ui-state-highlight",
-  tolerance: "intersect",
-  forcePlaceholderSize: true
-});
--- docs/fr_FR/changelog.md
+++ docs/fr_FR/changelog.md
@@ -2,8 +2,8 @@
 
 >**IMPORTANT**
 >
->Pour rappel s'il n'y a pas d'information sur la mise à jour, c'est que celle-ci concerne uniquement de la mise à jour de documentation, de traduction ou de texte.
+>S'il n'y a pas d'information sur la mise à jour, c'est que celle-ci concerne uniquement de la mise à jour de documentation, de traduction ou de texte.
 
 # 21/05/2023 - BETA
 
-- Creation
+- Création
